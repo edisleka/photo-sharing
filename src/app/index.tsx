8788371b2d17799aefc288fff5df6d33d35fd332
Edis Leka <edisleka@yahoo.gr>
2025-09-14 17:05:31 +0300

    Supabase Setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,18 @@
 import { Text, View } from 'react-native'
 import { Link } from 'expo-router'
+import { supabase } from '../lib/supabase'
+import { useEffect } from 'react'
 
 export default function HomeScreen() {
+  useEffect(() => {
+    supabase
+      .from('events')
+      .select('*')
+      .then(({ data }) => {
+        console.log(JSON.stringify(data, null, 2))
+      })
+  }, [])
+
   return (
     <View className='flex-1 items-center justify-center gap-5'>
       <Link href='/camera' className='text-white'>
